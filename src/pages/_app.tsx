461395d613e9345ef185536dae5579b722dacc22
steezy2401 <swggNuko.dev@gmail.com>
2022-03-11 00:34:15 +0200

    refactor: :art: app base
    Provided app base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,26 @@
+import { MantineProvider } from '@mantine/core';
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
-import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Seo />
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   );
 }
 
