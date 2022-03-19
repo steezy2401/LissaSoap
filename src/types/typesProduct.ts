23eedaa62e21406fa235ddb9d95df4aba1bdb816
steezy2401 <swggNuko.dev@gmail.com>
2022-03-19 11:17:18 +0200

    refactor: :pencil2: moved types
    Moved Product types to another file

---
+++ src/types/typesProduct.ts
@@ -0,0 +1,11 @@
+type ProductProps = {
+  name: string;
+  description: string;
+  price: number;
+  currencySign?: string;
+  hasDiscount?: boolean;
+  lastPrice?: number;
+  discountPercent?: number;
+};
+
+export default ProductProps;
