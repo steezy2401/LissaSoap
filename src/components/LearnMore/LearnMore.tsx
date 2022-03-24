42cf964f857dbcf7af0939dda2dd16afbcb85f6c
steezy2401 <swggNuko.dev@gmail.com>
2022-03-24 19:58:33 +0200

    feat: :sparkles: classname prop for LearnMore button

--- src/components/LearnMore/LearnMore.tsx
+++ src/components/LearnMore/LearnMore.tsx
@@ -1,32 +1,41 @@
 import React, { RefObject } from 'react';
 
-export default function LearnMore({
-  refToScroll,
-}: {
-  refToScroll: RefObject<HTMLButtonElement>;
-}) {
-  const executeScroll = () => {
-    if (refToScroll.current !== null) {
-      refToScroll.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+import clsxm from '@/lib/clsxm';
 
-  return (
-    <div
-      className='flex cursor-pointer select-none flex-col gap-5'
-      onClick={executeScroll}
-    >
-      <span className='text-lg'>Learn more...</span>
-      <div className='mouse_scroll'>
-        <div className='mouse hidden md:block'>
-          <div className='wheel'></div>
-        </div>
-        <div>
-          <span className='m_scroll_arrows unu'></span>
-          <span className='m_scroll_arrows doi'></span>
-          <span className='m_scroll_arrows trei'></span>
+type LearnMoreProps = {
+  refToScroll: RefObject<HTMLButtonElement>;
+} & React.ComponentPropsWithRef<'div'>;
+
+const LearnMore = React.forwardRef<HTMLButtonElement, LearnMoreProps>(
+  ({ className, refToScroll }) => {
+    const executeScroll = () => {
+      if (refToScroll.current !== null) {
+        refToScroll.current.scrollIntoView({ behavior: 'smooth' });
+      }
+    };
+
+    return (
+      <div
+        className={clsxm(
+          'flex cursor-pointer select-none flex-col gap-5',
+          className
+        )}
+        onClick={executeScroll}
+      >
+        <span className='text-lg'>Learn more...</span>
+        <div className='mouse_scroll'>
+          <div className='mouse hidden md:block'>
+            <div className='wheel'></div>
+          </div>
+          <div>
+            <span className='m_scroll_arrows unu'></span>
+            <span className='m_scroll_arrows doi'></span>
+            <span className='m_scroll_arrows trei'></span>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+export default LearnMore;
