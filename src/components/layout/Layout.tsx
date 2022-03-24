3a5a69d17e447ae04bc96515a623411f1e49cbc1
steezy2401 <swggNuko.dev@gmail.com>
2022-03-24 13:43:19 +0200

    feat: :lipstick: page transition

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,14 @@
+import { motion } from 'framer-motion';
 import * as React from 'react';
 
 import Footer from './Footer';
 import Header from './Header';
+
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+};
 
 export default function Layout({
   headerVariant = 'default',
@@ -13,7 +20,16 @@ export default function Layout({
   return (
     <>
       <Header variant={headerVariant} />
-      {children}
+      <motion.main
+        variants={variants} // Pass the variant object into Framer Motion
+        initial='hidden' // Set the initial state to variants.hidden
+        animate='enter' // Animated state to variants.enter
+        exit='exit' // Exit state (used later) to variants.exit
+        transition={{ type: 'linear' }} // Set the transition to linear
+        className=''
+      >
+        {children}
+      </motion.main>
       <Footer />
     </>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { MantineProvider } from '@mantine/core';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { ThemeProvider } from 'next-themes';
+import { useState } from 'react';
+import React from 'react';
 
 import '@/styles/globals.css';
 import '@/styles/gradientBackground.css';
@@ -11,14 +14,36 @@ import Seo from '@/components/Seo';
 import { mantineTheme } from '@/stylesheet';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = useState<boolean>(false);
+  React.useEffect(() => {
+    const handleStart = () => {
+      setPageLoading(true);
+    };
+    const handleComplete = () => {
+      setPageLoading(false);
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+  }, [router]);
+
   return (
     <>
-      <Seo />
-      <MantineProvider withNormalizeCSS theme={mantineTheme}>
-        <ThemeProvider defaultTheme='dark'>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </MantineProvider>
+      {pageLoading ? (
+        <div>Loading</div>
+      ) : (
+        <>
+          <Seo />
+
+          <ThemeProvider defaultTheme='dark'>
+            <MantineProvider withNormalizeCSS theme={mantineTheme}>
+              <Component {...pageProps} />
+            </MantineProvider>
+          </ThemeProvider>
+        </>
+      )}
     </>
   );
 }
