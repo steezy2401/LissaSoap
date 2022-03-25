d8c4146f10f21deb78d92dec639166db58ee2c2b
steezy2401 <swggNuko.dev@gmail.com>
2022-03-25 17:52:23 +0200

    fix: :bug: fixed AddToCart types

--- src/components/buttons/AddToCart/AddToCart.tsx
+++ src/components/buttons/AddToCart/AddToCart.tsx
@@ -3,59 +3,61 @@ import React from 'react';
 
 import clsxm from '@/lib/clsxm';
 
-type AddToCartProps = {
+interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   state: boolean;
   action?: () => void;
-} & React.ComponentPropsWithRef<'button'>;
+}
 
-const AddToCart = React.forwardRef<HTMLButtonElement, AddToCartProps>(
-  ({ className, state, action = () => void 0 }) => {
-    const buttonVariants = {
-      initial: {
-        scale: 1,
-      },
-      hover: {
-        scale: 1.05,
-      },
-      click: {
-        scale: 0.95,
-      },
-    };
+const AddToCart = ({
+  className,
+  state,
+  action = () => void 0,
+}: ButtonProps) => {
+  const buttonVariants = {
+    initial: {
+      scale: 1,
+    },
+    hover: {
+      scale: 1.05,
+    },
+    click: {
+      scale: 0.95,
+    },
+  };
 
-    const container = {
-      hidden: { opacity: 0, y: 20, transition: { duration: 0.2 } },
-      show: {
-        opacity: 1,
-        transition: {
-          staggerDirection: -1,
-        },
-        y: 0,
+  const container = {
+    hidden: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerDirection: -1,
       },
-    };
+      y: 0,
+    },
+  };
 
-    return (
+  return (
+    <motion.div
+      variants={container}
+      initial='hidden'
+      animate={state ? 'show' : 'hidden'}
+      className={clsxm(
+        'hidden w-full cursor-pointer select-none justify-center sm:flex',
+        className
+      )}
+      onClick={action}
+    >
       <motion.div
-        variants={container}
-        initial='hidden'
-        animate={state ? 'show' : 'hidden'}
-        className={clsxm(
-          'hidden w-full cursor-pointer select-none justify-center sm:flex',
-          className
-        )}
-        onClick={action}
+        initial='initial'
+        whileHover='hover'
+        whileTap='click'
+        variants={buttonVariants}
+        className='button-shadow-default w-40 rounded-md border-2 border-solid border-white bg-white p-2 text-center'
       >
-        <motion.div
-          initial='initial'
-          whileHover='hover'
-          whileTap='click'
-          variants={buttonVariants}
-          className='button-shadow-default w-40 rounded-md border-2 border-solid border-white bg-white p-2 text-center'
-        >
-          <span className='text-lg font-semibold text-black'>Add to cart</span>
-        </motion.div>
+        <span className='text-lg font-semibold text-black'>Add to cart</span>
       </motion.div>
-    );
-  }
-);
+    </motion.div>
+  );
+};
 
 export default AddToCart;
