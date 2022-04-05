94f9e2859d7ea86c4485b6783b7b9b5dc60b4c9c
Ivan Ilichev <swggnuko.dev@gmail.com>
2022-04-06 01:24:55 +0300

    feat: :recycle: layout now in _app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { IoLogoInstagram } from 'react-icons/io';
 import Button from '@/components/buttons/Button/Button';
 import LearnMore from '@/components/buttons/LearnMore/LearnMore';
 import ProductSlider from '@/components/elements/ProductSlider/ProductSlider';
-import Layout from '@/components/layout/Layout';
 
 import { fetchSlider } from '@/services/slider.services';
 
@@ -30,120 +29,113 @@ export default function HomePage({ slider }: { slider: Slider }) {
   const scrollTo = useRef(null);
 
   return (
-    <Layout headerVariant='homepage'>
-      <main>
-        <section>
-          <div className='flex min-h-screen flex-col items-center gap-10 text-center'>
-            <div className='flex w-11/12 flex-col gap-20 md:w-5/6 md:gap-10'>
-              <div className='hidden md:block'>
-                <Image src={logo} alt='logo' className='select-none' priority />
-              </div>
-              <div className='block md:hidden'>
-                <Image
-                  src={logoMobile}
-                  alt='logo'
-                  className='select-none'
-                  priority
-                />
-              </div>
-              <h1 className='hidden text-5xl font-extrabold md:block'>
+    <main>
+      <section>
+        <div className='flex min-h-screen flex-col items-center gap-10 text-center'>
+          <div className='flex w-11/12 flex-col gap-20 md:w-5/6 md:gap-10'>
+            <div className='hidden md:block'>
+              <Image src={logo} alt='logo' className='select-none' priority />
+            </div>
+            <div className='block md:hidden'>
+              <Image
+                src={logoMobile}
+                alt='logo'
+                className='select-none'
+                priority
+              />
+            </div>
+            <h1 className='hidden text-5xl font-extrabold md:block'>
+              Explore a beautiful world of{' '}
+              <span className='text-gradient font-extrabold'>soap</span> with us
+            </h1>
+            <h1 className='flex flex-col gap-2 text-4xl font-extrabold md:hidden'>
+              <span>
                 Explore a beautiful world of{' '}
-                <span className='text-gradient font-extrabold'>soap</span> with
-                us
-              </h1>
-              <h1 className='flex flex-col gap-2 text-4xl font-extrabold md:hidden'>
-                <span>
-                  Explore a beautiful world of{' '}
-                  <span className='text-gradient font-extrabold'>soap</span>
-                </span>
-                <span>with us</span>
-              </h1>
-            </div>
-            <LearnMore refToScroll={scrollTo} />
+                <span className='text-gradient font-extrabold'>soap</span>
+              </span>
+              <span>with us</span>
+            </h1>
           </div>
-        </section>
-        <section ref={scrollTo}>
-          <div className='flex min-h-screen flex-col items-center justify-center gap-10'>
-            <ProductSlider tabs={slider.tabs} sections={slider.sections} />
-            <Button
-              onClick={() => window.open('/soap', '_self')}
-              variant='filled'
-            >
-              View catalog
-            </Button>
-          </div>
-        </section>
-        <section className='layout'>
-          <div className='flex flex-col gap-24'>
-            <div className='justify-around  gap-10 md:flex'>
-              <div>
-                <h1 className='text-4xl md:text-5xl'>
-                  Create your own{' '}
-                  <span className='text-gradient-cyan-prurple font-extrabold'>
-                    custom
-                  </span>{' '}
-                  soap
-                </h1>
-                <div className='mb-8 mt-14 flex items-center justify-center md:hidden'>
-                  <div className='w-2/3'>
-                    <Image
-                      src={colorPicker}
-                      alt='logo'
-                      className='select-none'
-                    />
-                  </div>
+          <LearnMore refToScroll={scrollTo} />
+        </div>
+      </section>
+      <section ref={scrollTo}>
+        <div className='flex min-h-screen flex-col items-center justify-center gap-10'>
+          <ProductSlider tabs={slider.tabs} sections={slider.sections} />
+          <Button
+            onClick={() => window.open('/soap', '_self')}
+            variant='filled'
+          >
+            View catalog
+          </Button>
+        </div>
+      </section>
+      <section className='layout'>
+        <div className='flex flex-col gap-24'>
+          <div className='justify-around  gap-10 md:flex'>
+            <div>
+              <h1 className='text-4xl md:text-5xl'>
+                Create your own{' '}
+                <span className='text-gradient-cyan-prurple font-extrabold'>
+                  custom
+                </span>{' '}
+                soap
+              </h1>
+              <div className='mb-8 mt-14 flex items-center justify-center md:hidden'>
+                <div className='w-2/3'>
+                  <Image src={colorPicker} alt='logo' className='select-none' />
                 </div>
-                <span className='block text-lg text-[#828282] md:text-xl'>
-                  You can customize any soap in our store or create your own
-                  from scratch
-                </span>
-                <Button className='mt-8' variant='gradient'>
-                  Start building
-                </Button>
               </div>
-              <div className='hidden w-2/5 text-right md:block'>
-                <Image src={colorPicker} alt='logo' className='select-none' />
-              </div>
+              <span className='block text-lg text-[#828282] md:text-xl'>
+                You can customize any soap in our store or create your own from
+                scratch
+              </span>
+              <Button className='mt-8' variant='gradient'>
+                Start building
+              </Button>
             </div>
+            <div className='hidden w-2/5 text-right md:block'>
+              <Image src={colorPicker} alt='logo' className='select-none' />
+            </div>
+          </div>
 
-            <div className='justify-around gap-10 md:flex'>
-              <div className='order-2 shrink'>
-                <h1 className='text-4xl md:text-5xl'>
-                  Follow{' '}
-                  <span className='text-gradient-pink-prurple font-extrabold'>
-                    LissaSoap
-                  </span>{' '}
-                  on Instagram
-                </h1>
-                <div className='mb-8 mt-14 flex items-center justify-center md:hidden'>
-                  <div className='w-2/3'>
-                    <Image src={inst} alt='logo' className='select-none' />
-                  </div>
+          <div className='justify-around gap-10 md:flex'>
+            <div className='order-2 shrink'>
+              <h1 className='text-4xl md:text-5xl'>
+                Follow{' '}
+                <span className='text-gradient-pink-prurple font-extrabold'>
+                  LissaSoap
+                </span>{' '}
+                on Instagram
+              </h1>
+              <div className='mb-8 mt-14 flex items-center justify-center md:hidden'>
+                <div className='w-2/3'>
+                  <Image src={inst} alt='logo' className='select-none' />
                 </div>
-                <span className='block text-lg text-[#828282] md:text-xl'>
-                  Our Instagram is full of delightful photos of our soap
-                  collections. Follow us to stay tuned
-                </span>
-                <Button
-                  className='mt-8'
-                  variant='outline'
-                  icon={<IoLogoInstagram size={25} />}
-                  onClick={() =>
-                    window.open('https://www.instagram.com/', '_blank')
-                  }
-                >
-                  Follow us
-                </Button>
               </div>
-              <div className='order-1 hidden w-1/5 items-center justify-center md:flex'>
-                <Image src={inst} alt='logo' className='select-none' />
-              </div>
+              <span className='block text-lg text-[#828282] md:text-xl'>
+                Our Instagram is full of delightful photos of our soap
+                collections. Follow us to stay tuned
+              </span>
+              <Button
+                className='mt-8'
+                variant='outline'
+                icon={<IoLogoInstagram size={25} />}
+                onClick={() =>
+                  window.open('https://www.instagram.com/', '_blank')
+                }
+              >
+                Follow us
+              </Button>
+            </div>
+            <div className='order-1 hidden w-1/5 items-center justify-center md:flex'>
+              <Image src={inst} alt='logo' className='select-none' />
             </div>
           </div>
-        </section>
-        <div className='mb-24 mt-48'></div>
-      </main>
-    </Layout>
+        </div>
+      </section>
+      <div className='mb-24 mt-48'></div>
+    </main>
   );
 }
 
--- src/pages/soap.tsx
+++ src/pages/soap.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import PageTitle from '@/components/elements/PageTitle/PageTitle';
 import ProductGrid from '@/components/elements/ProductGrid/ProductGrid';
-import Layout from '@/components/layout/Layout';
 
 import { fetchProducts } from '@/services/products.services';
 
@@ -23,18 +22,16 @@ export default function SoapPage({ products }: SoapPageProps) {
   */
 
   return (
-    <Layout>
-      <main className='layout'>
-        <div className='mt-5'>
-          <PageTitle title='Soap' />
-        </div>
-        <div></div>
-        <div className='mb-24 mt-24'></div>
-        <section className='min-h-screen'>
-          <ProductGrid items={products} />
-        </section>
-      </main>
-    </Layout>
+    <main className='layout'>
+      <div className='mt-5'>
+        <PageTitle title='Soap' />
+      </div>
+      <div></div>
+      <div className='mb-24 mt-24'></div>
+      <section className='min-h-screen'>
+        <ProductGrid items={products} />
+      </section>
+    </main>
   );
 }
 
