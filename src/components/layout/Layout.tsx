249e4b1f5d3a2dcc46d6efab750d330d40997462
steezy2401 <swggNuko.dev@gmail.com>
2022-04-08 02:10:06 +0300

    feat: :dizzy: added a smooth page transition

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,23 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 
 import Footer from './Footer';
 import Header from './Header';
 
+/*
 const variants = {
   hidden: { opacity: 0 },
-  enter: { opacity: 1 },
+  enter: { opacity: 1, transition: { duration: 1 } },
   exit: { opacity: 0 },
+};
+*/
+
+const styles = {
+  content: 'content',
+  fadeIn: 'fadeInContent',
+  fadeOut: 'fadeOutContent',
 };
 
 export default function Layout({
@@ -19,20 +28,34 @@ export default function Layout({
 }) {
   const router = useRouter();
 
+  const [displayChildren, setDisplayChildren] = useState(children);
+  const [transitionStage, setTransitionStage] = useState<'fadeIn' | 'fadeOut'>(
+    'fadeOut'
+  );
+
+  useEffect(() => {
+    setTransitionStage('fadeIn');
+  }, []);
+
+  useEffect(() => {
+    if (children !== displayChildren) setTransitionStage('fadeOut');
+  }, [children, displayChildren]);
+
   return (
     <>
       <div className='gradient-opacity h-full w-full'>
         <Header variant={router.asPath == '/' ? 'homepage' : 'default'} />
         <AnimatePresence>
           <motion.main
-            variants={variants} // Pass the variant object into Framer Motion
-            initial='hidden' // Set the initial state to variants.hidden
-            animate='enter' // Animated state to variants.enter
-            exit='exit' // Exit state (used later) to variants.exit
-            transition={{ type: 'linear', duration: 1 }} // Set the transition to linear
-            className=''
+            onTransitionEnd={() => {
+              if (transitionStage === 'fadeOut') {
+                setDisplayChildren(children);
+                setTransitionStage('fadeIn');
+              }
+            }}
+            className={`${styles.content} ${styles[transitionStage]}`}
           >
-            {children}
+            {displayChildren}
           </motion.main>
         </AnimatePresence>
         <Footer />
