929cbfb8b6a4bdedb5a057b4cabef00eff418031
steezy2401 <swggNuko.dev@gmail.com>
2022-04-14 17:19:42 +0300

    feat: :sparkles: added filters Picker element

--- src/types/dropdown.types.ts
+++ src/types/dropdown.types.ts
@@ -1,3 +1,5 @@
+import { IconProps } from '@/types/icon.types';
+
 export interface IDropdownItems {
   id?: string;
   color?: string;
@@ -7,3 +9,35 @@ export interface IDropdownItems {
 }
 
 export type ItemsPosition = 'rtl' | 'ltr';
+
+export interface DropdownDefaultProps
+  extends React.ComponentPropsWithoutRef<'button'> {
+  animateIcon?: boolean;
+  Icon?: React.ComponentType<IconProps>;
+  itemsPosition?: ItemsPosition;
+}
+
+export interface DropdownPickerProps extends DropdownDefaultProps {
+  variant: 'picker';
+  activeItems?: string[];
+  handlePick?: (id: string) => void;
+  dropdownItems: IDropdownItems[];
+}
+
+export interface DropdownColorProps extends DropdownDefaultProps {
+  variant: 'color';
+  activeItems?: string[];
+  handlePick?: (id: string) => void;
+  dropdownItems: IDropdownItems[];
+}
+
+export interface DropdownRangeProps extends DropdownDefaultProps {
+  variant: 'range';
+  range?: { min: number; max: number };
+  handlePick?: (values: { min?: number; max?: number }) => void;
+}
+
+export type DropdownProps =
+  | DropdownPickerProps
+  | DropdownRangeProps
+  | DropdownColorProps;
