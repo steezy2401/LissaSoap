4d249aa3c1ee4667e9b889d7f2cb50b2983dd02e
steezy2401 <swggNuko.dev@gmail.com>
2022-04-15 13:31:49 +0300

    feat: :sparkles: added static data for filters

---
+++ src/static/order.static.ts
@@ -0,0 +1,8 @@
+import { TOrderItems } from '@/types/dropdown.types';
+
+const orderItems: TOrderItems[] = [
+  { field: 'name', title: 'Name', type: 'INC' },
+  { field: 'price', title: 'Price', type: 'DESC' },
+];
+
+export { orderItems };
---
+++ src/static/slider.static.ts
@@ -0,0 +1,8 @@
+import { TRange } from '@/types/dropdown.types';
+
+const sliderValues: TRange = {
+  min: 50,
+  max: 200,
+};
+
+export { sliderValues };
