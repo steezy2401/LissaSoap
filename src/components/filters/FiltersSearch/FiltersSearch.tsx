23898a57a8c776a610f768c465f3f0340661650c
steezy2401 <swggNuko.dev@gmail.com>
2022-04-26 16:53:40 +0300

    fix: :art: removed ref props from FiltersSearch

--- src/components/filters/FiltersSearch/FiltersSearch.tsx
+++ src/components/filters/FiltersSearch/FiltersSearch.tsx
@@ -12,79 +12,76 @@ import SearchIcon from '@/components/icons/SearchIcon';
 
 import { IconProps } from '@/types/icon.types';
 
-type FiltersSearchProps = {
+interface FiltersSearchProps {
   query: string;
   handleSearch?: (query: string) => void;
   buttonRef?: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
   animateIcon?: boolean;
   itemsPosition?: ItemsPosition;
   Icon?: React.ComponentType<IconProps>;
-} & React.ComponentPropsWithoutRef<'input'>;
+  className?: string;
+}
 
-const FiltersSearch = React.forwardRef<HTMLButtonElement, FiltersSearchProps>(
-  ({
-    query,
-    handleSearch = () => void 1,
-    className,
-    Icon = SearchIcon,
-    animateIcon = true,
-    itemsPosition = 'ltr',
-  }) => {
-    const { hovered, ref: hoveredRef } = useHover();
+export default function FiltersSearch({
+  query,
+  handleSearch = () => void 1,
+  className,
+  Icon = SearchIcon,
+  animateIcon = true,
+  itemsPosition = 'ltr',
+}: FiltersSearchProps) {
+  const { hovered, ref: hoveredRef } = useHover();
 
-    return (
-      <motion.div
+  return (
+    <motion.div
+      className={clsxm(
+        'rounded-corners-gradient-borders cursor-pointer rounded-2xl border-0',
+        className
+      )}
+    >
+      <div
+        ref={hoveredRef}
         className={clsxm(
-          'rounded-corners-gradient-borders cursor-pointer rounded-2xl border-0',
-          className
+          'relative flex w-full gap-3 rounded-xl bg-[#0B0B13] py-2 pl-3 ',
+          itemsPosition == 'rtl' ? 'pr-5' : 'pr-3'
         )}
       >
-        <div
-          ref={hoveredRef}
+        <motion.span
           className={clsxm(
-            'relative flex w-full gap-3 rounded-xl bg-[#0B0B13] py-2 pl-3 ',
-            itemsPosition == 'rtl' ? 'pr-5' : 'pr-3'
+            'leading-[0px] text-white',
+            itemsPosition == 'rtl' ? 'order-1' : 'order-2'
           )}
         >
-          <motion.span
-            className={clsxm(
-              'leading-[0px] text-white',
-              itemsPosition == 'rtl' ? 'order-1' : 'order-2'
-            )}
-          >
-            <Icon
-              size={28}
-              color='white'
-              animate={animateIcon}
-              animationState={hovered}
-            />
-          </motion.span>
-          <Input
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              handleSearch(e.target.value);
-            }}
-            variant='unstyled'
-            placeholder='Search...'
-            value={query}
-            className={clsxm(
-              'bg-[#0B0B13] font-primary text-xl font-semibold text-white',
-              itemsPosition == 'rtl' ? 'order-2' : 'order-1'
-            )}
-            styles={{
-              input: {
-                fontSize: '1.25rem',
-                lineHeight: 0,
-                height: '100%',
-                minHeight: 0,
-                color: '#F8F8FF',
-                fontWeight: 600,
-              },
-            }}
+          <Icon
+            size={28}
+            color='white'
+            animate={animateIcon}
+            animationState={hovered}
           />
-        </div>
-      </motion.div>
-    );
-  }
-);
-
-export default FiltersSearch;
+        </motion.span>
+        <Input
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            handleSearch(e.target.value);
+          }}
+          variant='unstyled'
+          placeholder='Search...'
+          value={query}
+          className={clsxm(
+            'bg-[#0B0B13] font-primary text-xl font-semibold text-white',
+            itemsPosition == 'rtl' ? 'order-2' : 'order-1'
+          )}
+          styles={{
+            input: {
+              fontSize: '1.25rem',
+              lineHeight: 0,
+              height: '100%',
+              minHeight: 0,
+              color: '#F8F8FF',
+              fontWeight: 600,
+            },
+          }}
+        />
+      </div>
+    </motion.div>
+  );
+}
