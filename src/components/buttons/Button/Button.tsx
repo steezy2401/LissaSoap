e76ef9671b67482b4394d8d181b9eb755ce627ca
steezy2401 <swggNuko.dev@gmail.com>
2022-04-26 16:39:11 +0300

    fix: :bug: removed ripple effect from button

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -36,20 +36,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ref,
     ...rest
   }) => {
-    const [coords, setCoords] = React.useState({ x: -1, y: -1 });
-    const [isRippling, setIsRippling] = React.useState(false);
-
-    React.useEffect(() => {
-      if (coords.x !== -1 && coords.y !== -1) {
-        setIsRippling(true);
-        setTimeout(() => setIsRippling(false), 300);
-      } else setIsRippling(false);
-    }, [coords]);
-
-    React.useEffect(() => {
-      if (!isRippling) setCoords({ x: -1, y: -1 });
-    }, [isRippling]);
-
     return (
       <MantineButton
         ref={ref}
@@ -70,9 +56,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           className
         )}
         onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-          const node = e.target as HTMLElement;
-          const rect = node.getBoundingClientRect();
-          setCoords({ x: e.clientX - rect.left, y: e.clientY - rect.top });
           onClick && onClick(e);
         }}
         sx={{
@@ -82,17 +65,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         }}
         {...rest}
       >
-        {isRippling ? (
-          <span
-            className='ripple'
-            style={{
-              left: coords.x,
-              top: coords.y,
-            }}
-          />
-        ) : (
-          ''
-        )}
         <span className='z-2 relative'>{children}</span>
       </MantineButton>
     );
