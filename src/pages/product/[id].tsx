a807716ddc345a47bbe7fe11ca7cfdb8a4c25bc3
steezy2401 <swggNuko.dev@gmail.com>
2022-05-03 19:44:51 +0300

    feat: :sparkles: polishing Product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,16 +1,9 @@
 import { Divider } from '@mantine/core';
-import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-import { Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 
 import PriceButton from '@/components/buttons/PriceButton/PriceButton';
 import Accordion from '@/components/elements/Accordion/Accrodion';
-import Breadcrumbs from '@/components/elements/Breadcrumbs/Breadcrumbs';
+import ImageDisplay from '@/components/elements/ImageDisplay/ImageDisplay';
 import Link from '@/components/elements/Link/Link';
 import ProductGrid from '@/components/elements/ProductGrid/ProductGrid';
 import SizePicker from '@/components/elements/SizePicker/SizePicker';
@@ -61,103 +54,97 @@ export default function ProductPage({
 
   return (
     <main>
-      <div className='rounded-3xl bg-[#070707] bg-opacity-70'>
-        <Swiper
-          pagination={{
-            type: 'fraction',
-          }}
-          navigation={true}
-          modules={[Pagination, Navigation]}
-          className='productSwiper'
-        >
-          {productData.images.map((item, key) => (
-            <SwiperSlide className='w-full py-10' key={`slide-${key}`}>
-              <div>
-                <div className='relative w-full pb-[90%]'>
-                  <Image
-                    src={item}
-                    alt='image'
-                    layout='fill'
-                    objectFit='contain'
-                  />
+      <ImageDisplay
+        variant='slider'
+        images={productData.images}
+        className='rounded-3xl bg-[#070707] bg-opacity-70 md:hidden'
+      />
+
+      <section className='layout flex-row gap-16 md:mt-7 md:flex md:w-full md:max-w-full md:px-10'>
+        <ImageDisplay
+          variant='grid'
+          images={productData.images}
+          className='hidden w-8/12 md:block'
+        />
+        <div className='md:w-4/12'>
+          <div className='top-28 flex flex-col gap-5 md:sticky '>
+            <div className='mt-10 flex flex-col gap-2 md:mt-0'>
+              {productData.inStock && (
+                <div className='text-lg font-bold uppercase text-green'>
+                  <span>In stock</span>
                 </div>
+              )}
+              <div className='flex flex-col'>
+                <span className='text-5xl font-extrabold'>
+                  {productData.name}
+                </span>
+                <span className='text-lg  text-gray'>
+                  {productData.description}
+                </span>
               </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <section className='layout'>
-        <div className='mt-5'>
-          <Breadcrumbs
-            customCrumbs={[{ title: 'Catalog', href: '/catalog' }]}
-          />
-        </div>
-        <div className='flex flex-col gap-5'>
-          <div className='mt-10 flex flex-col gap-2'>
-            {productData.inStock && (
-              <div className='text-lg font-bold uppercase text-green'>
-                <span>In stock</span>
-              </div>
-            )}
-            <div className='flex flex-col'>
-              <span className='text-5xl font-extrabold'>
-                {productData.name}
-              </span>
-              <span className='text-lg  text-gray'>
-                {productData.description}
-              </span>
             </div>
-          </div>
-          <div>
-            <Link href='/custom' variant='customize' Icon={CustomizeIcon}>
-              Customize
-            </Link>
-          </div>
-          <div className='mt-3 flex flex-col gap-3'>
-            <SizePicker
-              defaultValue='md'
-              changeHandler={(value) => setSelectedSize(value)}
-            />
-            <Link href='/info/sizing-guide' variant='default' Icon={RularIcon}>
-              Sizing guide
-            </Link>
-          </div>
-          <div className='mt-7 w-full'>
-            <PriceButton
-              fullWidth={true}
-              price={120}
-              isLoading={addToCart}
-              onClick={handleAddToCart}
-            >
-              Add to cart
-            </PriceButton>
-          </div>
-          <Divider className='my-7' size='sm' color='greyBorder' />
-          <div className='flex flex-col gap-3'>
-            <Link href='/info/shipping' variant='default' Icon={DollyIcon}>
-              Shipping & Prices
-            </Link>
-            <Link href='/support' variant='default' Icon={CommentIcon}>
-              Ask a question
-            </Link>
+            <div>
+              <Link href='/custom' variant='customize' Icon={CustomizeIcon}>
+                Customize
+              </Link>
+            </div>
+            <div className='mt-3 flex flex-col gap-3'>
+              <SizePicker
+                defaultValue='md'
+                changeHandler={(value) => setSelectedSize(value)}
+                className='min-w-min xl:w-8/12'
+              />
+              <Link
+                href='/info/sizing-guide'
+                variant='no-underline'
+                Icon={RularIcon}
+              >
+                Sizing guide
+              </Link>
+            </div>
+            <div className='mt-7 w-full xl:w-8/12'>
+              <PriceButton
+                fullWidth={true}
+                price={120}
+                isLoading={addToCart}
+                onClick={handleAddToCart}
+              >
+                Add to cart
+              </PriceButton>
+            </div>
+            <Divider className='my-7' size='sm' color='greyBorder' />
+            <div className='flex flex-col gap-3'>
+              <Link
+                href='/info/shipping'
+                variant='no-underline'
+                Icon={DollyIcon}
+              >
+                Shipping & Prices
+              </Link>
+              <Link href='/support' variant='no-underline' Icon={CommentIcon}>
+                Ask a question
+              </Link>
+            </div>
           </div>
         </div>
       </section>
       <section className='layout my-16'>
         <Accordion>
-          <Accordion.Title>
-            <h1>Information</h1>
-          </Accordion.Title>
-          <Accordion.Body>
-            <Accordion.Item lable='Creation process'>
-              Colors, fonts, shadows and many other parts are customizable to
-              fit your design needs
-            </Accordion.Item>
-            <Accordion.Item lable='Consist'>
-              Colors, fonts, shadows and many other parts are customizable to
-              fit your design needs
-            </Accordion.Item>
-          </Accordion.Body>
+          <div className='flex-row gap-20 md:flex'>
+            <Accordion.Title>
+              <h1>Information</h1>
+            </Accordion.Title>
+            <Accordion.Body>
+              <Accordion.Item lable='Creation process'>
+                Colors, fonts, shadows and many other parts are customizable to
+                fit your design needs
+              </Accordion.Item>
+              <Accordion.Item lable='Consist'>
+                Colors, fonts, shadows and many other parts are customizable to
+                fit your design needs
+              </Accordion.Item>
+            </Accordion.Body>
+          </div>
         </Accordion>
       </section>
       <section className='layout'>
