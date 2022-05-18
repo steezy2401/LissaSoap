fe26ae7844d2e418f9fb4e6e512c419d19d27ed6
Ivan Ilichev <swggnuko.dev@gmail.com>
2022-05-18 19:55:45 +0300

    feat: :sparkles: updated Filters Dropdown story

--- src/components/filters/FiltersDropdown/FiltersDropdown.stories.tsx
+++ src/components/filters/FiltersDropdown/FiltersDropdown.stories.tsx
@@ -2,6 +2,9 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import * as React from 'react';
 
 import FiltersDropdown from '@/components/filters/FiltersDropdown';
+import FiltersPicker from '@/components/filters/FiltersPicker';
+import FiltersRange from '@/components/filters/FiltersRange';
+import SettingsIcon from '@/components/Icon/SettingsIcon';
 
 export default {
   title: 'Components/Filters/FiltersDropdown',
@@ -14,6 +17,24 @@ export default {
   },
 } as ComponentMeta<typeof FiltersDropdown>;
 
+const flavors = [
+  { id: '1', title: 'Apple', description: 'Smooth apple smell', icon: '111' },
+  {
+    id: '2',
+    title: 'Orange',
+    description: 'Smooth orange smell',
+    icon: '111',
+  },
+  { id: '3', title: 'Rose', description: 'Smooth rose smell', icon: '111' },
+  { id: '4', title: 'Mint', description: 'Smooth mint smell', icon: '111' },
+  {
+    id: '5',
+    title: 'Porfume',
+    description: 'Smooth porfume smell',
+    icon: '111',
+  },
+];
+
 const Template: ComponentStory<typeof FiltersDropdown> = (args) => (
   <FiltersDropdown {...args} />
 );
@@ -21,5 +42,12 @@ const Template: ComponentStory<typeof FiltersDropdown> = (args) => (
 export const Picker = Template.bind({});
 Picker.args = {
   label: 'Picker',
-  children: <div></div>,
+  children: <FiltersPicker dropdownItems={flavors} />,
+};
+
+export const Range = Template.bind({});
+Range.args = {
+  label: 'Range',
+  Icon: SettingsIcon,
+  children: <FiltersRange range={{ min: 0, max: 300 }} />,
 };
