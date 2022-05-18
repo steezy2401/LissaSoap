6351853ad2385c87f932083dfd187ac4ded8ae7b
Ivan Ilichev <swggnuko.dev@gmail.com>
2022-05-18 17:00:17 +0300

    ci: :hammer: added component boilerplate creation script

--- plopfile.js
+++ plopfile.js
@@ -1,40 +1,35 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const inquirerFuzzyPath = require('inquirer-fuzzy-path');
-const storyGenerator = require('./src/generators/story');
-
 module.exports = function (plop) {
-  plop.setGenerator('stories', storyGenerator);
-
-  //#region  //*=========== Handlebars Helper ===========
-  /**
-   * Generate story component route
-   * @see https://stackoverflow.com/questions/41490076/capitalize-every-letter-after-and-characters
-   */
-  plop.setHelper('directoryCase', function (title) {
-    return title.replace(/(^|\/|-)(\S)/g, (s) => s.toUpperCase());
+  plop.setGenerator("component", {
+    description: "Create a component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Component's name:",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        templateFile: "templates/Component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
+        templateFile: "templates/Component.stories.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
+        templateFile: "templates/Component.test.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/index.tsx",
+        templateFile: "templates/index.tsx.hbs",
+      },
+    ],
   });
-
-  /**
-   * Remove 'src', and file name from path
-   */
-  plop.setHelper('getFolder', (path) => {
-    const split = path.split('\\');
-    // remove filename
-    split.pop();
-    if (split[0] === 'src') split.splice(0, 1);
-    return split.join('/');
-  });
-
-  /**
-   * Get file name from path, removing .tsx prefix
-   */
-  plop.setHelper('getName', (path) => {
-    const split = path.split('\\');
-    return split[split.length - 1].replace(/\.tsx$/, '');
-  });
-  //#endregion  //*======== Handlebars Helper ===========
-
-  //#region  //*=========== Inquirer Prompt ===========
-  plop.setPrompt('fuzzypath', inquirerFuzzyPath);
-  //#endregion  //*======== Inquirer Prompt ===========
 };
+
