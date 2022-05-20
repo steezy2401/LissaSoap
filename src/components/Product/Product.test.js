22968e99e75d36182dd5b9a45846ec5c16516f8f
Ivan Ilichev <swggnuko.dev@gmail.com>
2022-05-21 01:01:43 +0300

    test: :white_check_mark: added Product component test

--- src/components/Product/Product.test.js
+++ src/components/Product/Product.test.js
@@ -1,26 +1,50 @@
-import { render, screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+import '@/lib/__mocks__/matchMedia.mock';
+
+import { render } from '@/lib/test-utils';
 
 import Product from './Product';
 
-describe('App component', () => {
-  test('it renders', async () => {
-    render(
-      <Product
-        name='Bomb'
-        description='Vanilla, Purple'
-        price={120}
-        images={[
-          'https://i.ibb.co/qF5sWLh/bomb-big.png',
-          'https://i.ibb.co/FbZ2X7F/bomb2.png',
-          'https://i.ibb.co/w6R7Hvn/bomb3.png',
-          'https://i.ibb.co/qF5sWLh/bomb-big.png',
-        ]}
-      />
-    );
+const testProps = {
+  name: 'Bomb',
+  slug: 'bomb',
+  description: 'Soap in shape of bombs',
+  price: 120,
+  image: 'https://i.ibb.co/qF5sWLh/bomb-big.png',
+  hasDiscount: true,
+  discountPrice: 100,
+};
 
-    expect(screen.getByText(/bomb/i)).toBeInTheDocument();
-    expect(screen.getByText(/vanilla, purple/i)).toBeInTheDocument();
-    expect(screen.getByText(/120/i)).toBeInTheDocument();
+describe('Product', () => {
+  it('Is rendering', () => {
+    render(<Product {...testProps} />);
+    expect(screen.getByText(testProps.name)).toBeDefined();
+    expect(screen.getByText(testProps.description)).toBeDefined();
+    expect(screen.getByText(testProps.price)).toBeDefined();
+    expect(screen.getByText(testProps.discountPrice)).toBeDefined();
+    expect(screen.getByRole('link')).toHaveAttribute(
+      'href',
+      `/product/${testProps.slug}`
+    );
+  });
+
+  it('Test Add-to-Cart button', () => {
+    render(<Product {...testProps} />);
+    const addToCartButton = screen.getByRole('button', { name: 'Add to cart' });
+
+    expect(addToCartButton).toBeInTheDocument();
+    expect(addToCartButton).not.toBeVisible();
+  });
+
+  it('Test Add-to-Cart button hover', async () => {
+    const user = userEvent.setup();
+    render(<Product {...testProps} />);
+    const addToCartButton = screen.getByRole('button', { name: 'Add to cart' });
+
+    await user.hover(screen.getByTestId('product-root'));
+
+    await waitFor(() => expect(addToCartButton).toBeVisible());
   });
 });
